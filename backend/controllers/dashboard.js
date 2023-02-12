5f97228f79dc7c94f3bc757d3204f668b9d45783
Raahim <shraahim24@gmail.com>
2023-02-13 01:01:21 +0500

    "Better API Response Added" "The API now gives a more meaningful response when a user enters the dashboard"

--- backend/controllers/dashboard.js
+++ backend/controllers/dashboard.js
@@ -1,5 +1,15 @@
+const User = require("../models/User")
+
 module.exports = {
-    loadDashboard: (req, res) => {
-        res.status(200).json({page: "Dashboard"})
+    loadDashboard: async (req, res) => {
+        try{
+            //Getting User from DB
+            const user = await User.findById(req.user);
+            if(!user) throw new Error("User Not Found");
+
+            res.status(200).json({msg: `Welcome ${user.name}` , user})
+        }catch(error){
+            res.status(500).json({msg: error.message, error})
+        }
     }
 }
