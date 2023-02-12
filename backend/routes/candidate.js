53234ed2f47d0faccaffea301398fe422bcfd569
Raahim <shraahim24@gmail.com>
2023-02-12 22:24:50 +0500

    Middleware added to delete request, comments removed and restructured code

--- backend/routes/candidate.js
+++ backend/routes/candidate.js
@@ -1,27 +1,16 @@
 const express = require("express")
 
 //Controllers
-const {allCandidates, specificCandidate, addCandidate, updateCandidate} = require("../controllers/candidate");
+const {allCandidates, specificCandidate, addCandidate, updateCandidate, removeCandidate} = require("../controllers/candidate");
 
 //Creating Router
 const router = express.Router();
 
 //For Getting The Login Page
 router.get("/", allCandidates)
-
 router.get("/info/:id", specificCandidate)
-
-//For Sending Login Details For Server To Authenticate
 router.post("/", addCandidate)
-
 router.put("/update/:id", updateCandidate)
-
-router.delete("/delete/:id", (req, res) => {
-    try{
-
-    }catch(error){
-        res.status("400").json({message: error.message, error})
-    }
-})
+router.delete("/delete/:id", removeCandidate)
 
 module.exports = router;
