c5f30e50d472cc89cad671d38ee671e936b1a992
Raahim <shraahim24@gmail.com>
2023-02-13 01:04:28 +0500

    "JWT Authentication Added"

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -1,11 +1,44 @@
-module.exports = {
-    getLogin: (req, res) => {
-        res.status(200).json({title: "Login page"});
-    },
-    postLogin: () => {
-        //Validation And Verification Goes Here
+const jwt = require("jsonwebtoken")
+const crypto = require("crypto")
+const bcrypt = require("bcryptjs")
 
-        //Redirecting To Desired Page
-        res.redirect("/dashboard");
+//Model(s)
+const User = require("../models/User")
+
+module.exports = {
+    genToken: (id) => {
+        return jwt.sign({ id }, process.env.SECRET, { expiresIn: "1d" })
+    },
+    getLogin: (req, res) => {
+        try{
+            res.status(200).json({title: "Login page"}); 
+        }catch(error) {
+            res.status(400).json({msg: error.message, error})
+        }
+    },
+    postLogin: async (req, res) => {
+        try{
+            const {email, password} = req.body;
+            console.log(req.body)
+            //Checking for empty fields
+            if(!email || !password) throw new Error("Please Enter All Feilds");        
+    
+            //Now check the database for that user
+            const user = await User.findOne({email: email})
+            if(user == null) throw Error("User not found!");
+
+            //Checking for password validation
+            const passwordMatch = await bcrypt.compare(password, user.password);
+            if(!passwordMatch) throw new Error("Invalid Password You Hacker ;)")
+
+            //Generate JSON web token
+            const token = module.exports.genToken(user.id);
+
+            //Sending response if everything works
+            res.status(200).json({user, token})
+        }catch(error){
+            console.log(error)
+            res.status(400).json({msg: error.message, error})
+        }
     }
 }
