a46ed4e198d959ff5304ce3dc94ee0e1e84dc85a
Raahim <shraahim24@gmail.com>
2023-02-13 01:03:28 +0500

    "Added User Code" "Code to add a user to databse is written as a comment"

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,4 +1,6 @@
 const express = require("express")
+const bcrypt = require("bcryptjs")
+const User = require("../models/User")
 
 //Controllers
 const {getLogin, postLogin} = require("../controllers/login")
@@ -10,5 +12,21 @@ router.get("/", getLogin)
 
 //For Sending Login Details For Server To Authenticate
 router.post("/", postLogin)
+
+//Used Once to Create Users
+// router.post("/register",async (req, res) => {
+//     const {name, email, password} = req.body;
+
+//     const salt = await bcrypt.genSalt(10)
+//     const hash = await bcrypt.hash(password, salt)
+
+//     const user = await User.create({
+//         name,
+//         email,
+//         password: hash
+//     });
+    
+//     res.json({user})
+// })
 
 module.exports = router;
