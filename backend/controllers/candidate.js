89064ffc6c5c60f41266a243d21fc737ead90c9e
Sallar <106476322+sallar-ba@users.noreply.github.com>
2023-02-23 20:00:20 +0500

    Candidate Controller Updated

--- backend/controllers/candidate.js
+++ backend/controllers/candidate.js
@@ -1,5 +1,3 @@
-const asyncHandler = require("express-async-handler");
-
 //Loading Schemas
 const Candidate = require("../models/Candidate");
 const Speciality = require("../models/Speciality");
@@ -20,36 +18,37 @@ module.exports = {
     //To add a candidate to the DB
     addCandidate: asyncHandler(async (req, res) => {
         const {
-            name,
-            dob,
-            NIN_Number,
-            currAddress,
-            phoneNumber,
-            emailAddress,
-            candidateAccountFormLink,
-            Umbrella,
-            consultant,
-            candidatePayRate,
-            speciality
+          name,
+          dob,
+          NIN_Number,
+          currentAddress,
+          phoneNumber,
+          emailAddress,
+          candidateAccountFormLink,
+          umbrella,
+          consultant,
+          candidatePayRate,
+          specialty
         } = req.body;
 
-        
-        for(const key in req.body){
-            if(!req.body[key]) throw new Error("Please Enter All Feilds");
+        // Validate the request body
+        if (!name || !dob || !NIN_Number || !currentAddress || !phoneNumber || !emailAddress || !umbrella || !specialty) {
+          res.status(400).json({ message: "Please enter all required fields" });
+          return;
         }
 
         const newCandidate = new Candidate({
-            name,
-            dob,
-            NIN_Number,
-            currAddress,
-            phoneNumber,
-            emailAddress,
-            candidateAccountFormLink,
-            Umbrella,
-            consultant,
-            candidatePayRate,
-            speciality
+          name,
+          dob,
+          NIN_Number,
+          currentAddress,
+          phoneNumber,
+          emailAddress,
+          candidateAccountFormLink: candidateAccountFormLink || '',
+          umbrella,
+          consultant: consultant || '',
+          candidatePayRate: candidatePayRate || [],
+          specialty
         });
 
         const savedCandidate = await newCandidate.save();
@@ -59,50 +58,51 @@ module.exports = {
     //To update a candidate in the DB
     updateCandidate: asyncHandler(async (req, res) => {
         const {
-            name,
-            dob,
-            NIN_Number,
-            currAddress,
-            phoneNumber,
-            emailAddress,
-            candidateAccountFormLink,
-            Umbrella,
-            consultant,
-            candidatePayRate,
-            speciality
+          name,
+          dob,
+          NIN_Number,
+          currentAddress,
+          phoneNumber,
+          emailAddress,
+          candidateAccountFormLink,
+          umbrella,
+          consultant,
+          candidatePayRate,
+          specialty
         } = req.body;
 
         const candidate = await Candidate.findById(req.params.id);
 
         if (candidate) {
-            candidate.name = name || candidate.name;
-            candidate.dob = dob || candidate.dob;
-            candidate.NIN_Number = NIN_Number || candidate.NIN_Number;
-            candidate.currAddress = currAddress || candidate.currAddress;
-            candidate.phoneNumber = phoneNumber || candidate.phoneNumber;
-            candidate.emailAddress = emailAddress || candidate.emailAddress;
-            candidate.candidateAccountFormLink = candidateAccountFormLink || candidate.candidateAccountFormLink;
-            candidate.Umbrella = Umbrella || candidate.Umbrella;
-            candidate.consultant = consultant || candidate.consultant;
-            candidate.candidatePayRate = candidatePayRate || candidate.candidatePayRate;
-            candidate.speciality = speciality || candidate.speciality;
+          // Update the candidate fields if present in the request body
+          candidate.name = name || candidate.name;
+          candidate.dob = dob || candidate.dob;
+          candidate.NIN_Number = NIN_Number || candidate.NIN_Number;
+          candidate.currentAddress = currentAddress || candidate.currentAddress;
+          candidate.phoneNumber = phoneNumber || candidate.phoneNumber;
+          candidate.emailAddress = emailAddress || candidate.emailAddress;
+          candidate.candidateAccountFormLink = candidateAccountFormLink || candidate.candidateAccountFormLink;
+          candidate.umbrella = umbrella || candidate.umbrella;
+          candidate.consultant = consultant || candidate.consultant;
+          candidate.candidatePayRate = candidatePayRate || candidate.candidatePayRate;
+          candidate.specialty = specialty || candidate.specialty;
 
-            const updatedCandidate = await candidate.save();
-            res.status(200).json({ data: updatedCandidate });
+          const updatedCandidate = await candidate.save();
+          res.status(200).json({ data: updatedCandidate });
         } else {
             res.status(404).json({ message: "Candidate not found" });
         }
     }),
 
     //To delete a candidate from the DB
-    removeCandidate: asyncHandler(async (req, res) => {
+    deleteCandidateById: asyncHandler(async (req, res) => {
         const candidate = await Candidate.findById(req.params.id);
-
-        if (candidate) {
-            await candidate.remove();
-            res.status(200).json({ message: "Candidate removed" });
-        } else {
-            res.status(404).json({ message: "Candidate not found" });
+        if (!candidate) {
+          res.status(404).json({ message: "Candidate not found" });
+          return;
         }
-    })
-};
+        await candidate.remove();
+        res.status(200).json({ message: "Candidate removed" });
+    }),
+
+}
