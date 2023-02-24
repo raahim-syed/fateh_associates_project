653343400542793a75f08812a46582f1c0f00173
Sallar <106476322+sallar-ba@users.noreply.github.com>
2023-02-24 21:11:16 +0500

    Breakdown Form Redone Using ReachBootstrap Components

--- frontend/src/components/forms/BreakdownForm.jsx
+++ frontend/src/components/forms/BreakdownForm.jsx
@@ -1,97 +1,37 @@
-import { useState } from "react";
+import React, { useState } from 'react';
+import { Form, FormGroup, FormLabel, FormControl, Button } from 'react-bootstrap';
 
-function BreakdownForm({ candidates, umbrellas, onSubmit }) {
-  const [candidateId, setCandidateId] = useState("");
-  const [umbrellaId, setUmbrellaId] = useState("");
-  const [breakdownHours, setBreakdownHours] = useState([]);
-
-  const handleAddHours = () => {
-    setBreakdownHours([
-      ...breakdownHours,
-      { DayHrs: 0, NightHrs: 0, SundayHrs: 0 },
-    ]);
-  };
-
-  const handleRemoveHours = (index) => {
-    setBreakdownHours([...breakdownHours.slice(0, index), ...breakdownHours.slice(index + 1)]);
-  };
-
-  const handleHoursChange = (index, field, value) => {
-    const updatedHours = [...breakdownHours];
-    updatedHours[index][field] = value;
-    setBreakdownHours(updatedHours);
-  };
+const BreakdownForm = () => {
+  const [candidateName, setCandidateName] = useState('');
+  const [umbrella, setUmbrella] = useState('');
+  const [dayHrs, setDayHrs] = useState(0);
+  const [nightHrs, setNightHrs] = useState(0);
+  const [sundayHrs, setSundayHrs] = useState(0);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const data = { candidateId, umbrellaId, breakdownHours };
-    onSubmit(data);
-  };
+    // Handle form submission logic here
+  }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Candidate:
-        <select value={candidateId} onChange={(event) => setCandidateId(event.target.value)}>
-          <option value="">-- Select a candidate --</option>
-          {candidates.map((candidate) => (
-            <option key={candidate._id} value={candidate._id}>
-              {candidate.name}
-            </option>
-          ))}
-        </select>
-      </label>
-      <br />
-      <label>
-        Umbrella:
-        <select value={umbrellaId} onChange={(event) => setUmbrellaId(event.target.value)}>
-          <option value="">-- Select an umbrella --</option>
-          {umbrellas.map((umbrella) => (
-            <option key={umbrella._id} value={umbrella._id}>
-              {umbrella.name}
-            </option>
-          ))}
-        </select>
-      </label>
-      <br />
-      <h3>Breakdown Hours</h3>
-      {breakdownHours.map((hours, index) => (
-        <div key={index}>
-          <label>
-            Day Hours:
-            <input
-              type="number"
-              value={hours.DayHrs}
-              onChange={(event) => handleHoursChange(index, "DayHrs", event.target.value)}
-            />
-          </label>
-          <label>
-            Night Hours:
-            <input
-              type="number"
-              value={hours.NightHrs}
-              onChange={(event) => handleHoursChange(index, "NightHrs", event.target.value)}
-            />
-          </label>
-          <label>
-            Sunday Hours:
-            <input
-              type="number"
-              value={hours.SundayHrs}
-              onChange={(event) => handleHoursChange(index, "SundayHrs", event.target.value)}
-            />
-          </label>
-          <button type="button" onClick={() => handleRemoveHours(index)}>
-            Remove
-          </button>
-        </div>
-      ))}
-      <button type="button" onClick={handleAddHours}>
-        Add Hours
-      </button>
-      <br />
-      <br />
-      <button type="submit">Submit</button>
-    </form>
+    <Form onSubmit={handleSubmit}>
+      <FormGroup>
+        <FormLabel>Candidate Name</FormLabel>
+        <FormControl type="text" value={candidateName} onChange={(e) => setCandidateName(e.target.value)} required />
+      </FormGroup>
+      <FormGroup>
+        <FormLabel>Umbrella</FormLabel>
+        <FormControl type="text" value={umbrella} onChange={(e) => setUmbrella(e.target.value)} required />
+      </FormGroup>
+      <FormGroup>
+        <FormLabel>Breakdown Hours</FormLabel>
+        <FormControl type="number" placeholder="Day Hrs" value={dayHrs} onChange={(e) => setDayHrs(e.target.value)} required />
+        <FormControl type="number" placeholder="Night Hrs" value={nightHrs} onChange={(e) => setNightHrs(e.target.value)} required />
+        <FormControl type="number" placeholder="Sunday Hrs" value={sundayHrs} onChange={(e) => setSundayHrs(e.target.value)} required />
+      </FormGroup>
+      <Button type="submit">Submit</Button>
+    </Form>
   );
 }
+
+export default BreakdownForm;
