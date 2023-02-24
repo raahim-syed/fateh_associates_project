e676b2def8cd9be8aaf62739344bcc5a7f03d658
Raahim <shraahim24@gmail.com>
2023-02-24 17:44:21 +0500

    Redux store and query API initialized and configured

--- frontend/src/app/api/api.js
+++ frontend/src/app/api/api.js
@@ -4,12 +4,13 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 export const baseQuery = fetchBaseQuery({
     baseUrl: "http://localhost:5000",
     //To Get Cookie
-    credentials: "include",
+    // credentials: "include",
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
         if(token) {
             headers.set("authorization", `Bearer ${token}`)
         }
+        // headers.set("Content-type:", "application/json");
         return headers;
     }
 })
@@ -20,7 +21,6 @@ export const apiSlice = createApi({
     endpoints: builder => ({}),
     baseQuery,
 })
-console.log(apiSlice)
 
 // const baseQueryWithReAuth = async (args, api, extraOptions) => {
 //     let result = await baseQuery(args, api, extraOptions)
--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,14 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./api/api";
 import authReducer from "../features/auth/authSlice"
+import candidateReducer from "../features/candidates/candidatesSlice"
 
 
 export const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
         auth: authReducer,
+        candidate: candidateReducer,
     },
     middleware: getDefaultMiddleware => getDefaultMiddleware().concat(apiSlice.middleware),
-    devTools: true,
+    devTools: true, 
 })
 console.log(store)
