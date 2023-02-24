dddc1aac8ce52261badfb718421aa2d830229b8b
Sallar <106476322+sallar-ba@users.noreply.github.com>
2023-02-24 20:45:17 +0500

    umbrella Controller Updated

--- backend/controllers/umbrella.js
+++ backend/controllers/umbrella.js
@@ -1,89 +1,75 @@
-const asyncHandler = require("express-async-handler")
+const asyncHandler = require("express-async-handler");
 
 //Models
 const Invoice = require("../models/Invoice");
 const Umbrella = require("../models/Umbrella");
 
 module.exports = {
-    // Get all umbrellas
-    allUmbrellas: asyncHandler(async (req, res) => {
-        const umbrellas = await Umbrella.find({});
-        if(!umbrellas) res.status(200).json({ msg: "No Umbrellas Found!", umbrellas})
-        res.status(200).json({msg: "All Umbrellas" ,data: umbrellas});
-    }),
+  // Get all umbrellas
+  allUmbrellas: asyncHandler(async (req, res) => {
+    const umbrellas = await Umbrella.find({}).populate("invoices");
+    if (!umbrellas) res.status(200).json({ msg: "No Umbrellas Found!", umbrellas });
+    res.status(200).json({ msg: "All Umbrellas", data: umbrellas });
+  }),
 
-    // Get a specific umbrella by ID
-    specificUmbrella: asyncHandler(async (req, res) => {
-        const searchOptions = req.params.id;
-        const umbrella = await Umbrella.findById(searchOptions);
-        if(!umbrella) throw new Error("Could not find umbrella of id: " + searchOptions);
-        res.status(200).json({umbrella})
-    }),
+  // Get a specific umbrella by ID
+  specificUmbrella: asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const umbrella = await Umbrella.findById(id).populate("invoices");
+    if (!umbrella) throw new Error("Could not find umbrella of id: " + id);
+    res.status(200).json({ umbrella });
+  }),
 
-    // Add a new umbrella to the DB
-    addUmbrella: asyncHandler(async (req, res) => {
-        //Destructured Request Body
-        let {
-            name,
-            contactName,
-            address,
-            VATNumber,
-            companyNumber,
-            email,
-            additionalEmailAddresses,
-            phoneNumber,
-            bankDetails,
-            invoices,
-            payRate,
-          } = req.body;
+  // Add a new umbrella to the DB
+  addUmbrella: asyncHandler(async (req, res) => {
+    const {
+      name,
+      contactName,
+      address,
+      email,
+      additionalEmailAddresses,
+      phoneNumber,
+      invoices,
+    } = req.body;
 
-          console.log(additionalEmailAddresses)
+    // Validate required fields
+    if (!name || !contactName || !email || !phoneNumber) {
+      res.status(400).json({ msg: "Missing required field(s)" });
+      return;
+    }
 
-          //Parsing the objects
-          payRate = JSON.parse(payRate)
-          additionalEmailAddresses = JSON.parse(additionalEmailAddresses)
- 
-        // Validate required fields
-        if (!name || !contactName || !email || !phoneNumber || !payRate) res.status(400).json({ msg: "Missing required field(s)" });
+    // Create Umbrella object
+    const newUmbrella = new Umbrella({
+      name,
+      contactName,
+      address,
+      email,
+      additionalEmailAddresses,
+      phoneNumber,
+      invoices,
+    });
 
-        //Getting Invoices
+    // Save it to database
+    await newUmbrella.save();
 
-        //Create Umbrella Object
-        const newUmbrella = new Umbrella(
-            {name,
-            contactName,
-            address,
-            VATNumber,
-            companyNumber,
-            email,
-            additionalEmailAddresses,
-            phoneNumber,
-            bankDetails,
-            invoices,
-            payRate: [payRate]
-          });
+    res.status(200).json({ umbrella: newUmbrella });
+  }),
 
-        //Save it to database
-        await newUmbrella.save();
+  // Update an existing umbrella profile
+  updateUmbrella: asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const update = req.body;
+    const options = { new: true };
+    const result = await Umbrella.findByIdAndUpdate(id, update, options);
+    if (!result) throw new Error("Could not find umbrella of id: " + id);
+    res.status(200).json({ umbrella: result });
+  }),
 
-        res.status(200).json({umbrella: newUmbrella})
-    }),
-
-    // Update an existing umbrella profile
-    updateUmbrella: asyncHandler(async (req, res) => {
-        const searchOptions = req.params.id;
-        const update = req.body;
-        const options = { new: true };
-        const result = await Umbrella.findByIdAndUpdate(searchOptions, update, options);
-        if (!result) throw new Error("Could not find umbrella of id: " + searchOptions);
-        res.status(200).json({ umbrella: result });
-    }),
-
-    // Delete an umbrella profile
-    removeUmbrella: asyncHandler(async (req, res) => {
-        const searchOptions = req.params.id;
-        const result = await Umbrella.findByIdAndDelete(searchOptions);
-        if(!result) throw new Error("Could not find umbrella of id: " + searchOptions);
-        res.status(200).json({umbrella: result});
-    })
-}
+  // Delete an umbrella profile
+  removeUmbrella: asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const result = await Umbrella.findByIdAndDelete(id);
+    if (!result) throw new Error("Could not find umbrella of id: " + id);
+    res.status(200).json({ umbrella: result });
+  }),
+};
